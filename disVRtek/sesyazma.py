a7d9d042fe5a8c84884fd1b98df40e970fba7c20
yiğitcan <cakmak02273@gmail.com>
2022-12-09 13:39:49 +0300

    ad

---
+++ disVRtek/sesyazma.py
@@ -0,0 +1,7 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    audio = r.listen(source)
+    voice = r.recognize_google(audio, language = "en-US")
+    print(voice)
